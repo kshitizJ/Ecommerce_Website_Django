f4ec71387edde2c2332137caea9fcb336fe04d49
kshitizJ <coolkshitiz786@gmail.com>
2021-04-02 22:14:14 +0530

    added Product, order and Customer class in model and registered in admin panel

--- store/models.py
+++ store/models.py
@@ -33,3 +33,14 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
--- store/views.py
+++ store/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'templates/store/Store.html', context)
+
 
 def cart(request):
     context = {}
     return render(request, 'templates/store/Cart.html', context)
 
+
 def checkout(request):
-    context={}
-    return render(request,'templates/store/Checkout.html',context)
+    context = {}
+    return render(request, 'templates/store/Checkout.html', context)
