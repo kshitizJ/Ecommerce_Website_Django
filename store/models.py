c617d323dd205b18e07032d474675df871922da5
NidhiSinghh <singhnidhik2002@gmail.com>
2021-04-04 13:44:01 +0530

    Added shippingAdress class in models.py

--- store/models.py
+++ store/models.py
@@ -33,3 +33,15 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+
+class ShippingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	state = models.CharField(max_length=200, null=False)
+	zipcode = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
